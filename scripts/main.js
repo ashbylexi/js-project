52eaf56cd7048f5975915a1bbc185f26b1a586de
ashbylexi <ashbya@ufl.edu>
2020-11-13 13:34:12 -0500

    finished website

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+function Rotate1() {
+		var element = document.getElementById('image1');
+
+		if (element.className === "normal") {
+			element.className = "rotate";
+		}
+		else if ( element.className === "rotate") {
+			element.className = 'normal';
+		}
+	}
+  function Rotate2() {
+  		var element = document.getElementById('image2');
+
+  		if (element.className === "normal") {
+  			element.className = "rotate";
+  		}
+  		else if ( element.className === "rotate") {
+  			element.className = 'normal';
+  		}
+  	}
+    function Rotate3() {
+    		var element = document.getElementById('image3');
+
+    		if (element.className === "normal") {
+    			element.className = "rotate";
+    		}
+    		else if ( element.className === "rotate") {
+    			element.className = 'normal';
+    		}
+    	}
